796c763ccebab7eb4ec8c3453bcf8f7f5781deff
b0mbarnac <hominizlande@gmail.com>
2019-11-25 12:26:17 +0300

    12

--- src/com/company/Animator.java
+++ src/com/company/Animator.java
@@ -1,4 +1,17 @@
 package com.company;
 
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class Animator {
+
+    private Graphics graphics;
+    private Graphics screenGraphics;
+    private BufferedImage img;
+
+    public Animator(Graphics g){
+        screenGraphics = g;
+        img = new BufferedImage(BufferedImage.TYPE_3BYTE_BGR,750,1200);
+    }
+
 }
--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -1,16 +1,19 @@
 package com.company;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
 
     public Window(){
-        this.setBounds(100,100,1200,800);
+        this.setBounds(100, 100, 1200, 800);
         this.setVisible(true);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setLayout(null);
         JPanel panel = new JPanel();
-        panel.setBounds();
+        panel.setBounds(0,0, 1200, 750);
+        panel.setBackground(Color.WHITE);
         this.add(panel);
+        this.setVisible(true);
     }
 }
